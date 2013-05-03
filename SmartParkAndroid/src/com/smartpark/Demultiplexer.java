7672fc2de009d43da41f03107d529d0e533f3806
Saeed Ghasemi <basetutu@gmail.com>
2013-05-03 16:20:02 +0200

    changes

---
+++ SmartParkAndroid/src/com/smartpark/Demultiplexer.java
@@ -0,0 +1,42 @@
+package com.smartpark;
+
+import com.smartpark.interfaces.*;
+
+public class Demultiplexer implements OnMessageReceived {
+
+	@Override
+	public void messageReceived(String message) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

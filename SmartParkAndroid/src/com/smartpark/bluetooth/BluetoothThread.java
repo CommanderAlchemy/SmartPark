b7991b7320ece0a82d00129e872bbe87b2cfe09d
CommanderAlchemy <commander.alchemy@gmail.com>
2013-05-09 18:34:23 +0200

    Fixed typo in btThread

--- SmartParkAndroid/src/com/smartpark/bluetooth/BluetoothThread.java
+++ SmartParkAndroid/src/com/smartpark/bluetooth/BluetoothThread.java
@@ -7,8 +7,6 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 
 public class BluetoothThread extends Thread {
-	hg
-	
 	/** UUID for Serial Port Profile */
 	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 	

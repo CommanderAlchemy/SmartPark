34a338e84efb0b033c9a883d9cbcb85f8955247e
CommanderAlchemy <commander.alchemy@gmail.com>
2013-05-21 21:55:27 +0200

    Some Cleanups

--- SmartParkAndroid/src/com/smartpark/fragments/HistoryFragment.java
+++ SmartParkAndroid/src/com/smartpark/fragments/HistoryFragment.java
@@ -26,11 +26,11 @@ public class HistoryFragment extends Fragment {
 	// Debug Information, the boolean should be stored in a common settings file
 	// in the future!
 	private static boolean D = Ref.D;
-	private static final String TAG = "GPSFragment";
+	private static final String TAG = "HistoryFragment";
 	private String date = null;
 	private int day, year;
 	private String month;
-	Button btnFromDate;
+//	Button btnFromDate;
 	// This needs some fixing for code cleanup
 	public HistoryFragment() {
 		if (D)
@@ -43,8 +43,6 @@ public class HistoryFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.frag_history_view, container,
 				false);
-		
-		
 		
 		final Calendar cal = Calendar.getInstance();
 		day = cal.get(Calendar.DAY_OF_MONTH);
@@ -69,8 +67,10 @@ public class HistoryFragment extends Fragment {
 		date = day + " " + month + " " + year;
 		
 		
-		((Button) rootView.findViewById(R.id.btnFromDate)).setText(date);
-		((Button) rootView.findViewById(R.id.btnToDate)).setText(date);
+		Button btnFromDate = ((Button) rootView.findViewById(R.id.btnFromDate));
+		Button btnToDate = ((Button) rootView.findViewById(R.id.btnToDate));
+		btnFromDate.setText(date);
+		btnToDate.setTag(date);
 
 		// DELETE THIS LATER
 		// TextView dummyTextView = (TextView) rootView

2023f319d11e2c824cb075080db9b807cb43f1d0
CommanderAlchemy <commander.alchemy@gmail.com>
2013-05-23 22:51:33 +0200

    Added ParkingLots

---
+++ Server/src/tables/ParkingLots.java
@@ -0,0 +1,11 @@
+package tables;
+
+import database.Database;
+
+public class ParkingLots extends Database {
+
+	public ParkingLots(String dbName) {
+		super(dbName);
+		// TODO Auto-generated constructor stub
+	}
+}

5fd5eadb369abf184c67210d8c1fbde3bb982730
CommanderAlchemy <commander.alchemy@gmail.com>
2013-05-24 12:17:50 +0200

    changed to abstract class

--- Server/src/database/Database.java
+++ Server/src/database/Database.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Database {
+abstract public class Database {
 	private String dbName;
 	private Connection connection;
 	private ResultSet result;

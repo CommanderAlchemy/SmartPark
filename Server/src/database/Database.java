eb61002ec56a9e042ee8201149ac9c0176cfe6f4
CommanderAlchemy <commander.alchemy@gmail.com>
2013-05-24 03:34:47 +0200

    Minor fixes

--- Server/src/database/Database.java
+++ Server/src/database/Database.java
@@ -45,6 +45,7 @@ abstract public class Database {
 
 	/**
 	 * Get connection to the database
+	 * 
 	 * @return
 	 */
 	private Connection getConnection() {
@@ -58,6 +59,7 @@ abstract public class Database {
 
 	/**
 	 * Set connection to the database
+	 * 
 	 * @param c
 	 */
 	private void setConnection(Connection c) {
@@ -67,25 +69,28 @@ abstract public class Database {
 	/**
 	 * Create table in the database
 	 */
-	public void createTable(String tblName, String[] columns, String[] columnTypes, boolean[] notNull) {
-		
+	public void createTable(String tblName, String[] columns,
+			String[] columnTypes, boolean[] notNull) {
+
 		// check for spaces in tablename
-		if(tblName.contains(" ")){
+		if (tblName.contains(" ")) {
 			System.out.println("You may not use [space] in table name.");
 			return;
 		}
 		// Check for equal string array sizes
-		if(columns.length != columnTypes.length || columnTypes.length != notNull.length){
+		if (columns.length != columnTypes.length
+				|| columnTypes.length != notNull.length) {
 			System.out.println("The arrays are not equa in length");
 			return;
 		}
 		String sql = "CREATE TABLE " + tblName + " (ID INTEGER PRIMARY KEY,";
-		
-		for(int i = 0 ; i < columns.length ; i++){
-			sql += columns[i] + " " + columnTypes[i] + ((notNull[i])?" NOT NULL":"");
-			if(i != columns.length -1){
+
+		for (int i = 0; i < columns.length; i++) {
+			sql += columns[i] + " " + columnTypes[i]
+					+ ((notNull[i]) ? " NOT NULL" : "");
+			if (i != columns.length - 1) {
 				sql += ",\n";
-			}else{
+			} else {
 				sql += ")";
 			}
 		}
@@ -96,7 +101,7 @@ abstract public class Database {
 		} catch (SQLException e1) {
 			System.out.println("[ERROR] During Create New Customer Table:");
 			System.err.println(e1.getClass().getName() + ": " + e1.getMessage());
-		}finally{
+		} finally {
 			closeConnection();
 		}
 	}
@@ -135,8 +140,8 @@ abstract public class Database {
 	 * @param whatValue
 	 *            what new value
 	 */
-	public void updateTableData(String searcCol, String searchValue, String whatCol,
-			String whatValue) {
+	public void updateTableData(String searcCol, String searchValue,
+			String whatCol, String whatValue) {
 	}
 
 	/**
@@ -165,7 +170,7 @@ abstract public class Database {
 	public String toString() {
 		return null;
 	}
-	
+
 	public static void main(String[] args) {
 		Database.createTable();
 	}

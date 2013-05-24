329dca7489252bc6cc107774f6692f317c269e6f
CommanderAlchemy <commander.alchemy@gmail.com>
2013-05-24 04:33:07 +0200

    Parking Lots Mostly created

--- Server/src/tables/ParkingLots.java
+++ Server/src/tables/ParkingLots.java
@@ -1,16 +1,94 @@
 package tables;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import database.Database;
 
 public class ParkingLots extends Database {
 
 	private long ID;
-	private 
+	private long price;
+	private String ticketHours;
+	private String freeHours;
+	private ArrayList<String> longitude;
+	private ArrayList<String> latitude;
 	
+	private static String dbName = "test";
+	private static String tblName = "ParkingLots";
 	
 	
 	public ParkingLots(String dbName) {
 		super(dbName);
-		// TODO Auto-generated constructor stub
 	}
+
+
+	public long getID() {
+		return ID;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(long price) {
+		this.price = price;
+	}
+
+
+	public String getTicketHours() {
+		return ticketHours;
+	}
+
+
+	public void setTicketHours(String ticketHours) {
+		this.ticketHours = ticketHours;
+	}
+
+
+	public String getFreeHours() {
+		return freeHours;
+	}
+
+
+	public void setFreeHours(String freeHours) {
+		this.freeHours = freeHours;
+	}
+
+
+	public ArrayList<String> getLongitude() {
+		return longitude;
+	}
+
+
+	public void setLongitude(ArrayList<String> longitude) {
+		this.longitude = longitude;
+	}
+
+
+	public ArrayList<String> getLatitude() {
+		return latitude;
+	}
+
+
+	public void setLatitude(ArrayList<String> latitude) {
+		this.latitude = latitude;
+	}
+
+
+	@Override
+	public String toString() {
+		/* @formatter:off */
+		return "ID:"				+ this.ID				 	+ ";"
+				+ "deviceID:"		+ this.price			 	+ ";"
+				+ "ssNbr:"			+ this.ticketHours			+ ";"
+				+ "longitude:"		+ this.freeHours 			+ ";"
+				+ "latitude:"		+ this.longitude.toString()	+ ";"
+				+ "startStamp:" 	+ this.latitude.toString()	+ ";";
+		/* @formatter:on */
+	}
+	
+	
+	
 }

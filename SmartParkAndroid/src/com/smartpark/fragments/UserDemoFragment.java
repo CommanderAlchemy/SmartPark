05838028a6697f7cfa8157fba9d87d5484f7b2bc
Saeed Ghasemi <basetutu@gmail.com>
2013-05-31 04:25:58 +0200

    Implementing demo buttons for rold, tommy and kristina

--- SmartParkAndroid/src/com/smartpark/fragments/UserDemoFragment.java
+++ SmartParkAndroid/src/com/smartpark/fragments/UserDemoFragment.java
@@ -1,14 +1,26 @@
 package com.smartpark.fragments;
 
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.smartpark.R;
 import com.smartpark.activities.MainActivity;
+import com.smartpark.background.BackgroundOperationThread;
+import com.smartpark.background.Ref;
+import com.smartpark.gps.GPSReceiver;
 
 /**
  * BluetoothFragment, this holds the bluetooth side of the project and will show
@@ -22,11 +34,28 @@ public class UserDemoFragment extends Fragment {
 	// Debug Information, the boolean should be stored in a common settings file
 	// in the future!
 	private static boolean D = MainActivity.D;
+	private static SharedPreferences mainPreference;
 	private static final String TAG = "DemoFragment";
 
-	// This needs some fixing for code cleanup
-	public static final String ARG_SECTION_NUMBER = "section_number";
-
+	private View.OnClickListener onClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Log.e(TAG, "++ onClick ++");
+			if (v instanceof Button) {
+				switch (v.getId()) {
+				case R.id.btnTogglePark:
+					myVib.vibrate(50);
+					
+					
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	};
+	private Vibrator myVib;
+	
 	public UserDemoFragment() {
 		if (D)
 			Log.d(TAG, "Fragment: " + this.toString() + " Loaded");
@@ -38,9 +67,54 @@ public class UserDemoFragment extends Fragment {
 		Log.i(TAG, "++ onCreateView ++");
 		View rootView = inflater.inflate(R.layout.frag_demo_view, container,
 				false);
-
+		
+		myVib = (Vibrator) getActivity().getSystemService(
+				Activity.VIBRATOR_SERVICE);
+		
+		mainPreference = getActivity().getSharedPreferences("MainPreference", Context.MODE_PRIVATE);
+		
 		return rootView;
 	}
+	
+	
+	
+	public static boolean startParkDemo(String licensePlate, String carModel, String deviceID) {
+		Log.e(TAG, "++ startPark ++");
+
+		String demoPark = "DemoPark;";
+		String ssNbr = mainPreference.getString("ssNbr", "error");
+		if (!ssNbr.equals("error")) {
+			Location location = GPSReceiver.getLocation();
+			if (location == null) {
+				return false;
+			}
+			
+			Log.e(TAG, "location: " + location.getLatitude() + " " + location.getLongitude());
+			
+			Calendar cal = Calendar.getInstance();
+			long startTimestamp = cal.getTimeInMillis();
+			
+			// DemoPark;ssNbr:55.3452324:26.3423423:2342133424:0:ADT-435:Renault:0:0:deviceID
+			// param-size = 9
+			demoPark += ssNbr + ":" + location.getLatitude() + ":"
+					+ location.getLongitude() + ":" + startTimestamp + ":0:"
+					+ licensePlate + ":" + carModel + ":0:0:" + deviceID;
+
+			Log.e(TAG, "--> Send parking request:\n" + demoPark);
+			
+			BackgroundOperationThread.startParkString = demoPark.split(";")[1].split(":");
+
+			mainPreference.edit().putString("StartPark", demoPark).apply();
+
+			BackgroundOperationThread.sendByTCP(demoPark);
+			return true;
+		} else {
+			Toast.makeText(Ref.activeActivity, "Error,  please login again",
+					Toast.LENGTH_LONG).show();
+			return false;
+		}
+	}
+	
 
 
 }// ============================================================================
